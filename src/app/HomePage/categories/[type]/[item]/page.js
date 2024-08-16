387bb1c2193cff021157daeb85b5c1392f3b0324
woozai <wooozai@gmail.com>
2024-08-17 02:06:11 +0300

    added pages for tags and buttons

--- src/app/HomePage/categories/[type]/[item]/page.js
+++ src/app/HomePage/categories/[type]/[item]/page.js
@@ -1,19 +1,20 @@
 'use server'
 import Head from 'next/head'
-import Posts from '../../../components/posts'
-import NewPost from '../../../components//newPost'
+import Posts from '../../../../components/posts'
 
 export default async function HomePage({ params }) {
+  const { type, item } = params
+
   return (
     <div className="min-h-screen flex-auto w-full  ">
       <Head>
         <title>Tag Page</title>
       </Head>
       <div className="flex justify-center items-center my-4">
-        <h3 className="font-bold text-2xl">{`Tag Result: ${params.tag}`}</h3>
+        <h3 className="font-bold text-2xl">{`${type} Result: ${item}`}</h3>
       </div>
       <div className="">
-        <Posts keyPost={'all'} tag={params.tag} />
+        <Posts keyPost={'all'} item={item} type={type} />
       </div>
     </div>
   )
