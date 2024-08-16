2a29ddbe65e67e83cfa25d6033623c9081203368
woozai <wooozai@gmail.com>
2024-08-17 00:21:49 +0300

    new page

---
+++ src/app/HomePage/tags/[tag]/page.js
@@ -0,0 +1,18 @@
+'use server'
+import Head from 'next/head'
+import Posts from '../components/posts'
+import NewPost from '../components/newPost'
+
+export default async function HomePage() {
+  return (
+    <div className="min-h-screen flex-auto w-full  ">
+      <Head>
+        <title>Lobby</title>
+      </Head>
+      <div className="">
+        <NewPost />
+        <Posts keyPost={'all'} />
+      </div>
+    </div>
+  )
+}
