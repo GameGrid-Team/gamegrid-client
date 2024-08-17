64f784862aaa87e4fff558c042bab65a296d8a7d
liel26 <118679526+liel26@users.noreply.github.com>
2024-08-17 15:02:50 +0300

    placeholder

--- src/app/HomePage/Profile/[id]/edit/page.js
+++ src/app/HomePage/Profile/[id]/edit/page.js
@@ -364,6 +364,7 @@ export default function ProfilePage({ params }) {
                   name="password"
                   value={userData.password}
                   onChange={handleChange}
+                  placeholder="Enter your password"
                   className="w-full px-4 py-2 border-b-2 border-gray-300 rounded-none text-green-200 placeholder-gray-400 focus:border-b-4 focus:border-green-500 focus:outline-none transition-all duration-300 ease-in-out bg-transparent"
                 />
               </div>
@@ -377,6 +378,7 @@ export default function ProfilePage({ params }) {
                   name="confirmPassword"
                   value={userData.confirmPassword}
                   onChange={handleChange}
+                  placeholder="Repeat the password"
                   className="w-full px-4 py-2 border-b-2 border-gray-300 rounded-none text-green-200 placeholder-gray-400 focus:border-b-4 focus:border-green-500 focus:outline-none transition-all duration-300 ease-in-out bg-transparent"
                 />
               </div>
